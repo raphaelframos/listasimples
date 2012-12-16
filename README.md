42b836b0ca8013636944d24cd78fdf8f21fd5330
raphaelframos <raphaelframos@gmail.com>
2012-12-15 22:13:57 -0200

    Finalizando tutorial, refatorando o codigo e adicionando clique no item escolhido

---
+++ README.md
@@ -0,0 +1,2 @@
+Tutorial com a criação de uma lista simples, usando um ArrayList<String>() e mostrando o item clicado da lista.
+Target: 2.3.3
--- src/br/com/powell/listasimples/ListaSimples.java
+++ src/br/com/powell/listasimples/ListaSimples.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListaSimples extends Activity {
 	
@@ -17,11 +21,24 @@ public class ListaSimples extends Activity {
         setContentView(R.layout.lista_simples);
         
         listaSimples = (ListView) findViewById(R.id.listView1);
-        ArrayList<String> guitarras = criaNomeDeGuitarras();
+        definindoAdaptador();
+        listaSimples.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> item, View arg1, int posicao,
+					long arg3) {
+				String guitarra = (String) item.getItemAtPosition(posicao);
+				Toast.makeText(ListaSimples.this, "Eu gosto da guitarra: " + guitarra, 
+						Toast.LENGTH_LONG).show();
+			}
+		});
+    }
+
+	private void definindoAdaptador() {
+		ArrayList<String> guitarras = criaNomeDeGuitarras();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, 
         		android.R.layout.simple_list_item_1, guitarras);
         listaSimples.setAdapter(adapter);
-    }
+	}
 
 	private ArrayList<String> criaNomeDeGuitarras() {
 		ArrayList<String> guitarras = new ArrayList<String>();
